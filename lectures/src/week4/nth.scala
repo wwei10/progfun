a37f899f6794f42051439c62bbe6ca0448b701b2
Wei Wei <fraternite@live.cn>
2014-06-21 22:20:34 -0700

    Add polymorphism examples.

---
+++ lectures/src/week4/nth.scala
@@ -0,0 +1,17 @@
+import week4._
+
+object nth {
+  def main(args: Array[String]) {
+    def nth[T](n: Int, xs: List[T]): T =
+      if (xs.isEmpty) throw new IndexOutOfBoundsException
+      else if (n == 0) xs.head
+      else nth(n - 1, xs.tail)
+
+    val list = new Cons(1, new Cons(2, new Cons(3, new Nil)))
+    println(nth(0, list))
+    println(nth(1, list))
+    println(nth(2, list))
+    println(nth(3, list))
+    println(nth(-1, list))
+  }
+}
