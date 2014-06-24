28860c054331fda46bf0b73893a1cef3dd5a44be
Wei Wei <fraternite@live.cn>
2014-06-23 23:45:09 -0700

    Use eclipse to write scala code.

---
+++ lectures/README.md
@@ -0,0 +1,8 @@
+## Usage
+
+```sbt
+>>> compile # (compile source code)
+>>> run     # (select the one you want to run)
+```
+
+If you use eclipse, please first run ```eclipse``` in sbt then import it to eclipse.
---
+++ lectures/src/main/scala/week4/Expr.scala
@@ -0,0 +1,22 @@
+package week4
+
+trait Expr
+case class Number(n: Int) extends Expr
+case class Sum(e1: Expr, e2: Expr) extends Expr
+
+object Eval {
+  def eval(e: Expr): Int = e match {
+    case Number(n) => n
+    case Sum(e1, e2) => eval(e1) + eval(e2)
+  }
+  
+  def show(e: Expr): String = e match {
+    case Number(n) => n.toString
+    case Sum(e1, e2) => "(" + show(e1) + " + " + show(e2) + ")"
+  }
+  
+  def main(args: Array[String]) {
+    println(show(Sum(Number(1), Sum(Number(2), Number(3)))))
+    println(eval(Sum(Number(10), Number(14))))
+  }
+}
