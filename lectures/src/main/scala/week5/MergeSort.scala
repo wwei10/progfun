4639c13c1a346d432b3ffd8bd2c6f495d2ea3782
Wei Wei <fraternite@live.cn>
2014-06-27 20:39:08 -0700

    Parameterize merge sort.

--- lectures/src/main/scala/week5/MergeSort.scala
+++ lectures/src/main/scala/week5/MergeSort.scala
@@ -1,18 +1,20 @@
 package week5
 
+import math.Ordering
+
 object MergeSort {
   
-  def msort(xs: List[Int]): List[Int] = {
+  def msort[T](xs: List[T])(implicit ord: Ordering[T]): List[T] = {
     val n = xs.length / 2
     if (n == 0) xs
     else {
-      def merge(xs: List[Int], ys: List[Int]): List[Int] = {
+      def merge(xs: List[T], ys: List[T]): List[T] = {
         (xs, ys) match {
           case (List(), List()) => List()
           case (List(), ys1) => ys1
           case (xs1, List()) => xs1
           case (x :: xs1, y :: ys1) =>
-            if (x < y) x :: merge(xs1, ys)
+            if (ord.lt(x, y)) x :: merge(xs1, ys)
             else y :: merge(xs, ys1)
         }
       }
@@ -24,6 +26,15 @@ object MergeSort {
   def main(args: Array[String]): Unit = {
     val list = List(9,8,7,6,5,4,3,2,1,0)
     println(msort(list))
+    
+    val fruits = List("apple", "banana", "pineapple", "orange")
+    println(msort(fruits))
+    
+    object PairOrdering extends Ordering[(Int, String)] {
+      def compare(a: (Int, String), b: (Int, String)) = a._2 compare b._2
+    }
+    
+    val pairs = List((1, "z"), (2, "y"), (3, "x"))
+    println(msort(pairs)(PairOrdering))
   }
-
 }
